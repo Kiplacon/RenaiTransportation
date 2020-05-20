af5c50b8e384b4fc7bb760f931913552fc83b2fe
Kiplacon <XiaoXian95@gmail.com>
2020-05-19 20:34:19 -0400

    Add files via upload

---
+++ migrations/RT-0.2.0.lua
@@ -0,0 +1,22 @@
+for _, force in pairs(game.forces) do
+  force.reset_recipes()
+  force.reset_technologies()
+
+  -- create tech/recipe table once
+  local techs = force.technologies
+  local recipes = force.recipes
+  if techs["fast-inserter"].researched then
+    recipes["FastThrowerInserterRecipie"].enabled = true
+	recipes["FilterThrowerInserterRecipie"].enabled = true
+  end
+  
+  if techs["automation"].researched then
+    recipes["LongHandedThrowerInserterRecipie"].enabled = true
+  end
+  
+  if techs["stack-inserter"].researched then
+    recipes["StackThrowerInserterRecipie"].enabled = true
+	recipes["StackFilterThrowerInserterRecipie"].enabled = true
+  end  
+
+end
